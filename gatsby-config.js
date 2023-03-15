4503e92c54a23b6ffacdee7831f5eb6aca87ded9
Luna <gabrielluna2011002@gmail.com>
2023-03-14 21:22:15 -0300

    lesson-16 code: gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,21 @@
 module.exports = {
   plugins: [
     'gatsby-transformer-remark',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 const styles = require("../styles/home.module.css");
 
-export default function Home() {
+
+export default function Home({ data }) {
+  const banner = getImage(data.file)
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,8 +16,17 @@ export default function Home() {
           <p>UX designer & web developer based if Manchester</p>
           <Link className={styles.btn} to="/projects">My Portifolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="Site Banner" style={{ maxWidth: '100%' }}/>
+        <GatsbyImage image={ banner } alt="image" />
       </section>
     </Layout>
   );
 }
+
+export const query = graphql`
+query BannerImage {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      gatsbyImageData
+    }
+  }
+}`;
